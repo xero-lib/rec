e78b4145ba5270bc7aa73d351ff32fe273bbb264
thoth@damocles <jonathankurtz@protonmail.com>
2025-04-06 15:37:19 -0500

    Modularized output

---
+++ src/interface/output.rs
@@ -0,0 +1,7 @@
+pub mod print {
+    pub fn instructions() {
+        println!(
+            "Press enter to stop recording. Optionally, enter a name for the output file. If no name is supplied, the time/date at which the file was saved will be the name."
+        );
+    }
+}
