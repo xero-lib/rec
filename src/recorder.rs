47a5f5e51aa2481a1e980a8464f7eed0502b7cdc
thoth@damocles <jonathankurtz@protonmail.com>
2025-04-06 15:36:56 -0500

    Implemented custom types and structs, consolidated std use

--- src/recorder.rs
+++ src/recorder.rs
@@ -1,9 +1,9 @@
 mod audio;
 pub use audio::AudioRecorder;
 
-use std::fmt::Debug;
-use std::marker::PhantomData;
 use std::{
+    fmt::Debug,
+    marker::PhantomData,
     sync::{Arc, Mutex},
     thread::{self, Thread},
 };
@@ -14,17 +14,25 @@ use file_io::Extension;
 pub type RecFn<T> = Arc<Box<dyn Fn(Arc<Mutex<Vec<T>>>) + Send + Sync>>;
 pub type SaveFn<T> = Arc<Box<dyn Fn(Arc<Mutex<Vec<T>>>, Option<String>) + Send + Sync>>;
 
-// vs OnceLock? vs other?
 #[derive(Clone)]
+pub struct RecorderData<T> {
+    pub inner: Arc<Mutex<Vec<T>>>
+}
+
+impl<T> RecorderData<T> {
+    pub fn new() -> RecorderData<T> {
+        Self { inner: Arc::new(Mutex::new(Vec::<T>::new())) }
+    }
+}
+
 pub struct NotRecording;
-#[derive(Clone)]
 pub struct Recording;
 
 pub struct Recorder<T, State>
 where
     T: Clone,
 {
-    data: Option<Arc<Mutex<Vec<T>>>>,
+    data: Option<RecorderData<T>>,
     state: PhantomData<State>,
     recorder_type: Extension,
     recording_thread: Option<Thread>,
@@ -38,7 +46,7 @@ where
 {
     pub fn stop_recording(self, file_name: Option<String>) -> Recorder<T, NotRecording> {
         self.recording_thread.as_ref().unwrap().unpark();
-        (self.save_fn)(self.data.unwrap(), file_name);
+        (self.save_fn)(self.data.unwrap().inner, file_name);
 
         Recorder::<T, NotRecording> {
             data: None,
@@ -57,7 +65,7 @@ where
     Mutex<Vec<T>>: Send + Sync,
 {
     pub fn record(self) -> Result<Recorder<T, Recording>, Box<dyn std::error::Error>> {
-        let clone = self.data.clone().unwrap();
+        let clone = self.data.clone().unwrap().inner;
         let func = self.record_fn.clone();
         let recording_thread = Some(thread::spawn(move || func(clone)).thread().clone());
 
