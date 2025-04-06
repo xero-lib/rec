55044e64a9b90a6de60c4b63724a10ab307d77f2
thoth@damocles <jonathankurtz@protonmail.com>
2025-04-06 15:37:59 -0500

    Moved to ExitCode returns

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 mod interface;
 mod recorder;
+mod macros;
 
-use interface::input::read_line;
+use interface::{ input::read_line, output::print};
 use recorder::AudioRecorder as Recorder;
-use std::{error::Error, ops::Not};
+use std::{error::Error, process::ExitCode};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<ExitCode, Box<dyn Error>> {
     let rec = Recorder::new().record()?;
-    println!(
-        "Press enter to stop recording. Optionally, enter a name for the output file. If no name is supplied, the time/date at which the file was saved will be the name."
-    );
-    let name = read_line();
-    rec.stop_recording(name.is_empty().not().then_some(name));
-    Ok(())
+    print::instructions();
+    rec.stop_recording(some_nempty!(read_line()));
+
+    Ok(ExitCode::SUCCESS)
 }
